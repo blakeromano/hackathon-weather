f7b8c16bd3fc711e33b72c4e5c9af534df88c547
boyoon-c <boyoonchang@gmail.com>
2021-08-06 16:39:39 -0700

    Change the file extension and make separage pages folder

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./index.css";
-import App from "./pages/App/App";
+import App from "./App";
+
+
 //import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
