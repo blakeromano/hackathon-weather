74d9579c63ac788e3dc66cbc93c00acdfa5b3bfe
Blake R <blakeromano19@gmail.com>
2021-08-06 23:23:23 -0400

    reset mode

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import React, { Component } from 'react';
 import { Route, Link} from 'react-router-dom'
 import './App.css';
-import About from './Pages/About'
-import Home from './Pages/Home'
-import WeatherResult from './Pages/WeatherResult'
-import SpaceResult from './Pages/SpaceResult'
 import NavBar from './Components/NavBar'
 import Footer from './Components/Footer'
 
@@ -17,54 +13,7 @@ class App extends Component {
     return (
       <>
       <NavBar /> 
-      
-        {/* <nav>
-          <ul>
-          <Link to="/">Home</Link><br/>
-          <Link to="/about">About</Link>
-          </ul>
-        </nav> */}
-        <header>
-          React Weather CRUD
-        </header>
-        <section className='home-weather'> 
-              <header>Insert weather clip</header> 
-
-          </section> 
-
-          <section className='home-space'>
-              <header>Insert space  clip</header> 
-
-
-
-
-            </section>
-
-
-            <section className='home-space'>
-              <header>Insert Section Header</header> 
-
-
-
-
-            </section>
-        <Route exact path='/results' render={() => 
-          <SearchResult />
-        }/>
-        <Route exact path='/about' render={() => 
-          <About />
-        }/>
-        <Route exact path='/weather/' render={() => 
-          <WeatherResult />
-        }/>
-        <Route exact path='/space' render={() => 
-          <SpaceResult />
-        }/>
-        <Route exact path='/' render={() => 
-          <Home />
-        }/>
-
-        <Footer />
+      <Footer />
       </>
     );
   }
