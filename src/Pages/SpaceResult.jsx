e9ba1b86f7a630419e8e06665f46b5edeb6fbfdc
boyoon-c <boyoonchang@gmail.com>
2021-08-07 11:22:21 -0700

    Add title and description of the space images

--- src/Pages/SpaceResult.jsx
+++ src/Pages/SpaceResult.jsx
@@ -32,11 +32,20 @@ class SpaceResult extends Component{
       // if the array of spacePhoto is null then return search field
       // else return the spacePhotos
       <>
-      <h1>DISPLAY SPACE RESULTS</h1>
+      <Form onSubmit={this.handleSubmit}>
+          <Form.Group className="mb-3" >
+            <Form.Label>Search</Form.Label>
+            <Form.Control type="input" onChange={this.handleChange} name="search" placeholder='Provide serach query' />
+          </Form.Group>
+          <Button variant="primary">
+          Search
+          </Button>
+        </Form>
+      {/* <h1>DISPLAY SPACE RESULTS</h1>
       <form onSubmit={this.handleSubmit}>
         <input onChange={this.handleChange} type="text" name="search"/>
         <button>submit</button>
-      </form>
+      </form> */}
 
       <h1> 
         Search results:
@@ -46,12 +55,23 @@ class SpaceResult extends Component{
         {/* {this.props.spaceData[1]?.links[0]?.href} */}
         
         {this.props.spaceData?.map((img,idx) =>{
-          // console.log(img.links)
+          console.log(img?.data[0])
           // console.log(img.links[0])
-          console.log(idx,img?.links)
+          // console.log(idx,img?.links)
+          // wrapped around with another if statement of checking whether there is spaceData
+          if (this.props.spaceData){console.log("no pictures")}
           if (img.links){
             return(
-            <img src={img?.links[0]?.href} />
+            <div>
+              <img src={img?.links[0]?.href} />
+              <div className="fs-6">
+                Title: {img?.data[0]?.title}<br />
+                Date created:{img?.data[0].date_created}<br />
+                {img?.data[0].description_508 ? `Description: ${img?.data[0]?.description_508}`:""} <br />
+                {img?.data[0].secondary_creater ? `Secondary Creater: ${img?.data[0]?.secondary_creater}`:""} <br />
+              </div>
+            </div>
+            
           )
           }
           
