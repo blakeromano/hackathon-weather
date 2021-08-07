33e0da88197c21fb1b728a1d8689e635f89276c5
Nick Rodriguez <nickrrodriguez@gmail.com>
2021-08-06 22:18:26 -0400

    navbar

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./index.css";
 import App from "./App";
-import * as serviceWorker from "./serviceWorker";
+// import * as serviceWorker from "./serviceWorker";
 
 
 //import * as serviceWorker from "./serviceWorker";
